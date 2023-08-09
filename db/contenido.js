1e74a19e7a21c17129a026a8c247614d8c07bd98
Luciano Bustos <bustosluciano89@gmail.com>
2023-08-09 20:06:45 -0300

    Test add logo

---
+++ db/contenido.js
@@ -0,0 +1,9 @@
+[
+    {
+        "category": "Frameworks",
+        "Name": "Tailwind",
+        "Description": "Comprende las necesidades y deseos de tus usuarios antes de comenzar a diseñar en Figma.",
+        "logo": "",
+        "link": "https://tailwindcss.com/"
+    },
+]
