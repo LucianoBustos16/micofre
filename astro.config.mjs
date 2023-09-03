e1bc0da2952aa71d3867764d802eb9708eb8e02f
Luciano Bustos <bustosluciano89@gmail.com>
2023-09-03 15:45:47 -0300

    Add Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-    scripts: [
-        // Agrega el soporte para JavaScript
-        {
-          type: 'module',
-          src: '/src/utils.js',
-        },
-      ],
-      integrations: [tailwind()],
+  scripts: [
+  // Agrega el soporte para JavaScript
+  {
+    type: 'module',
+    src: '/src/utils.js'
+  }],
+  integrations: [tailwind(), partytown()]
 });
